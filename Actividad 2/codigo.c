190bd6fc531d3e7e3bbfa362750362c7b7c1a0c9
nuria-garcia415 <nuria.garcia415@comunidadunir.net>
2026-05-11 18:33:35 +0200

    Update codigo.c

--- Actividad 2/codigo.c
+++ Actividad 2/codigo.c
@@ -17,6 +17,7 @@
 #define NUMPIXELS 8
 #define TEMP_SETPOINT 25.0
 #define ZONA_MUERTA 3.0 //range from -3 to +3
+#define DISTANCIA_PRESENCIA 200 // Umbral en cm para detectar usuario
 
 //objects creation
 DHT dht(DHTPIN, DHT22);
@@ -35,8 +36,12 @@ void setup() {
   IrReceiver.begin(IR_PIN);
   tira.begin();
   
+  // Configuración pins ultrasonidos (Añadido del segundo código)
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
   ascensor.write(0); //floor = 0
-  lcd.print("SISTEMA ACME v2");
+  lcd.print("Actividad 2");
   delay(2000);
   lcd.clear();
 }
@@ -48,20 +53,33 @@ void loop() {
   int valorLuz = analogRead(LDR_PIN);
   int luzPct = map(valorLuz, 1023, 0, 0, 100);
 
+  //presence measurement
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  float distancia = pulseIn(ECHO_PIN, HIGH) * 0.034 / 2;
+
   //illumnination configuration
   int ledsActivos = 0;
-  if (luzPct < 80) { // Si hay poca luz natural
-    ledsActivos = map(luzPct, 80, 0, 0, 8);
+  //leds will turn on only if there's someone less than 200 cm close and light level is low
+  if (distancia > 0 && distancia < DISTANCIA_PRESENCIA) { 
+    if (luzPct < 80) { 
+      ledsActivos = map(luzPct, 80, 0, 0, 8);
+    }
+  } else {
+    ledsActivos = 0; //turn off when no one is close
   }
   
   tira.clear();
   for(int i = 0; i < ledsActivos; i++) {
-    tira.setPixelColor(i, tira.Color(200, 200, 200)); //white light
+    tira.setPixelColor(i, tira.Color(200, 200, 200)); 
   }
   tira.show();
 
   //temperature configuration
-  //T > 28 activa frio; T < 22 activa calor
+  //T > 28 activa frio; T < 22 activa calor)
   String estadoClima = "NORMAL";
   if (t > (TEMP_SETPOINT + ZONA_MUERTA)) {
     estadoClima = "FRIO ON";
@@ -72,13 +90,12 @@ void loop() {
   //IR remote configuration
   if (IrReceiver.decode()) {
     int comando = IrReceiver.decodedIRData.command;
-    // Mapeo estándar de botones Wokwi
-    if (comando == 104) viajar(0); //0
-    if (comando == 48)  viajar(1); //1
-    if (comando == 24)  viajar(2); //2
-    if (comando == 122) viajar(3); //3
-    if (comando == 16)  viajar(4); //4
-    if (comando == 56)  viajar(5); //5
+    if (comando == 104) viajar(0); 
+    if (comando == 48)  viajar(1); 
+    if (comando == 24)  viajar(2); 
+    if (comando == 122) viajar(3); 
+    if (comando == 16)  viajar(4); 
+    if (comando == 56)  viajar(5); 
     IrReceiver.resume();
   }
 
@@ -91,13 +108,12 @@ void viajar(int planta) {
   int angulo = map(planta, 0, 5, 0, 180);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("SUBIENDO A P:"); lcd.print(planta);
+  lcd.print("MOVIENDO A P:"); lcd.print(planta);
   ascensor.write(angulo);
   delay(2000); //artificial time that it takes to go to a different floor
   plantaActual = planta;
 }
 
-//update LCD
 void actualizarLCD(float t, float h, String clima) {
   lcd.setCursor(0, 0);
   lcd.print("P:"); lcd.print(plantaActual);
